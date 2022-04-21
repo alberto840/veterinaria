c1075326873e4f0372f01eb3c201eec1d5aadd01
Alberto840 <alberto.flores@ucb.edu.bo>
2022-04-21 15:23:35 -0400

    actualizar el formulario

--- nuevopedido.php
+++ nuevopedido.php
@@ -5,15 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="styles/contactos.css">
-    <title>SHOW TICKET</title>
+    <title>VetFoca</title>
   </head>
-  <body data-spy="scroll" data-target="#navbar" data-offset="57" style="background-color: #F9DBBF; color: white;">   
-    <nav id="header" class="navbar navbar-expand-lg sticky-top navbar-dark" style="background-color: #440B1C; color:
+  <body data-spy="scroll" data-target="#navbar" data-offset="57" style="background-color: #f0f0af  ; color: white;">   
+    <nav id="header" class="navbar navbar-expand-lg sticky-top navbar-dark" style="background-color: #9dbcbc; color:
     white;">
       <div class="container fs-5">
         <a class="navbar-brand" href="#">
-        <img src="images/ticketlogo.jpg" alt="Pedidos logo" style="width: 190px; height: 70px;">
-          SHOW TICKET
+        <img src="https://mundovets.com/wp-content/uploads/2021/10/Logotipo-para-clinica-veterinaria-scaled.jpg" alt="Pedidos logo" style="width: 190px; height: 70px;">
+        VetFoca
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -27,7 +27,7 @@
         <div class="row">
         <br><br><br>
           <div class="col text-uppercase text-center">
-            <h2 style="color: black;">REGISTRAR PEDIDO</h2>
+            <h2 style="color: black;">REGISTRAR CITA</h2>
           </div>
         </div>
         <div class="row">
@@ -40,42 +40,42 @@
               </div>
               <div class="form-row">
                 <div class="form-group col-12 col-md-18">
-                  <input type="text" class="form-control" placeholder="Producto" name="trans">
+                  <input type="text" class="form-control" placeholder="Causa de la cita medica" name="trans">
                 </div>
               </div>
               <div class="form-row">
                 <div class="form-group col-12 col-md-18">
-                  <input type="text" class="form-control" placeholder="Cantidad" name="prod">
+                  <input type="text" class="form-control" placeholder="sintomas" name="prod">
                 </div>
               </div>  
-              <p>Costo</p>            
+              <p style="color: black">Datos del animalito</p>            
               <div class="form-row">                
                 <div class="form-group col-12 col-md-4">
-                  <input type="text" class="form-control" placeholder="Precio" name="pre">
+                  <input type="text" class="form-control" placeholder="Especie" name="pre">
                 </div>
                 <div class="form-group col-12 col-md-4">
-                  <input type="text" class="form-control" placeholder="Precio de Envío" name="env">
+                  <input type="text" class="form-control" placeholder="Nombre del animal" name="env">
                 </div>
                 <div class="form-group col-12 col-md-4">
-                  <input type="text" class="form-control" placeholder="Total" name="tot">
+                  <input type="text" class="form-control" placeholder="Edad animal" name="tot">
                 </div>
               </div>
-              <p>Tiempo de entrega</p>  
+              <p style="color: black">Tiempo de la cita</p>  
               <div class="form-row">
                 <div class="form-group col-12 col-md-6">
-                  <input type="text" class="form-control" placeholder="Fecha" name="ha">
+                  <input type="text" class="form-control" placeholder="Fecha de cita" name="ha">
                 </div>
                 <div class="form-group col-12 col-md-6">
-                  <input type="text" class="form-control" placeholder="Hora Entrega" name="he">
+                  <input type="text" class="form-control" placeholder="Hora de cita" name="he">
                 </div>
               </div>
               <br><br>
               <div class="form-row">
                 <div class="col">
-                  <button  class="btn btn-block" style="background-color: #440B1C; color: white;" name="crear">Registrar</button>
+                  <button onclick="myFunction()" class="btn btn-block" style="background-color: #ff370f; color: white;" name="crear">Registrar</button>
                 </div>
                 <div class="col">
-                  <a href="index.php" class="btn btn-block" style="background-color: #2C0D00; color: white;">Atras</a>
+                  <a href="index.php" class="btn btn-block" style="background-color: #332717; color: white;">Atras</a>
                 </div>
               </div>
             </form>
@@ -86,6 +86,12 @@
         </div>
       </div>
     </section>
+
+    <script>
+      function myFunction() {
+      alert("¡La cita fue registrada, te estaremos esperando");
+    }
+</script>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
